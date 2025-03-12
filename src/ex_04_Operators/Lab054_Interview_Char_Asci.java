2d59b20fc955648fd8228b425549dc535bc5c6f6
debad <debadwaipayan10@gmail.com>
2025-03-12 21:24:20 +0530

    12th March Task and class notes

---
+++ src/ex_04_Operators/Lab054_Interview_Char_Asci.java
@@ -0,0 +1,15 @@
+package ex_04_Operators;
+
+public class Lab054_Interview_Char_Asci {
+    public static void main(String[] args) {
+        char a1 = 'A'; // ASCII -> 65
+        char b1 = 'B'; // 66
+        System.out.println(a1 + b1);
+
+        System.out.println('A' == 65);
+        short s = 10;
+        char c = 'A';//65
+        System.out.println(c+s);
+        // a1+b1 - 65+66 -> 131
+    }
+}
---
+++ src/ex_04_Operators/Task_12th_March_MathFunc.java
@@ -0,0 +1,23 @@
+package ex_04_Operators;
+
+public class Task_12th_March_MathFunc {
+    public static void main(String[] args) {
+
+        int X = 10, Y = 10, Z = 10;
+
+//        Method	Description	Example	Output
+//        Math.abs(x)	Absolute value	Math.abs(-10)	10
+//        Math.sqrt(x)	Square root	Math.sqrt(25)	5.0
+//        Math.pow(x, y)	Power (x^y)	Math.pow(2, 3)	8.0
+//        Math.max(x, y)	Maximum of two numbers	Math.max(5, 10)	10
+//        Math.min(x, y)	Minimum of two numbers	Math.min(5, 10)	5
+//        Math.round(x)	Rounds to nearest integer	Math.round(4.6)	5
+//        Math.ceil(x)	Rounds up	Math.ceil(4.2)	5.0
+//        Math.floor(x)	Rounds down	Math.floor(4.9)	4.0
+//        Math.random()	Generates a random number (0.0 - 1.0)	Math.random()	0.XX
+
+       double result = Math.cbrt((Math.pow(X,2))+(Math.pow(Y,2))-(Math.abs(Z)));
+
+        System.out.println("The Value of this problem is = "+ result);
+    }
+}
