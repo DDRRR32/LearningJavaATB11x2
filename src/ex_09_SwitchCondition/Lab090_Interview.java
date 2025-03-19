9452d356957b65ec6970b7d56771374f8ccd4de0
debad <debadwaipayan10@gmail.com>
2025-03-19 22:18:56 +0530

    19th March Task and class notes

---
+++ src/ex_09_SwitchCondition/Lab090_Interview.java
@@ -0,0 +1,9 @@
+package ex_09_SwitchCondition;
+
+public class Lab090_Interview {
+    public static void main(String[] args) {
+//        boolean b = true;
+//        switch (b) {
+//        }
+    }
+}
---
+++ src/ex_10_For_Loops/Lab099_For_LOOP_VAR_NAME.java
@@ -0,0 +1,9 @@
+package ex_10_For_Loops;
+
+public class Lab099_For_LOOP_VAR_NAME {
+    public static void main(String[] args) {
+        for (int deba = 0; deba < 10; deba++) {
+            System.out.println(deba);
+        }
+    }
+}
